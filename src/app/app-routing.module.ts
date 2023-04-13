2a486427574e83172e6218073b3480f406c90a49
xhanti-fatyela <xmanmrcool@gmail.com>
2023-04-13 17:25:56 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+    { path: '', component: NotesListComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
