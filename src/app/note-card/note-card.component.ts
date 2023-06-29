f751823c0a72614eef5d5621ff717d2a6b9854e4
xhanti-fatyela <xmanmrcool@gmail.com>
2023-06-29 12:13:58 +0200

    fixed bugs

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -6,36 +6,33 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2, Input, Output, Eve
   styleUrls: ['./note-card.component.scss']
 })
 export class NoteCardComponent implements OnInit {
-
   @Input() title: string;
   @Input() body: string;
   @Input() link: string;
 
-  @Output('delete') deleteEvent: EventEmitter<void> = new EventEmitter<void>();
+  @Output('delete') delete: EventEmitter<void> = new EventEmitter<void>();
 
-  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
-  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+  @ViewChild('truncator', { static: true }) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText: ElementRef<HTMLElement>;
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
-    // work out if there is a text overflow and if not, then hide the truncator
+    this.checkTextOverflow();
+  }
 
-    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
-    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+  private checkTextOverflow() {
+    const style = getComputedStyle(this.bodyText.nativeElement);
+    const viewableHeight = parseInt(style.getPropertyValue('height'), 10);
 
     if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
-      // if there is a text overflow, show the fade out truncator
       this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
     } else {
-      // else (there is a text overflow), hide the fade out truncator
       this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
     }
   }
 
-
   onXButtonClick() {
-    this.deleteEvent.emit();
+    this.delete.emit();
   }
-
 }
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./notes-list.component.scss']
 })
 export class NotesListComponent {
+
+  notes: Note[] = new Array<Note>()
+
+constructor(private notesService: NotesService) {}
+
+  ngOnInit() {
+      
+   this.notes = this.notesService.getAll()
+
+
+  }
+
+  deleteNote(id: number) {
+     
+    this.notesService.delete(id)
+
+  }
 
 }
